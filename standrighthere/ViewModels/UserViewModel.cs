112d57d8764bcde0017a824be43000473dd0a71f
Chuma Nnaji <cnnaji2003@gmail.com>
2013-08-11 23:01:01 -0700

    Fixing user control to show real data

--- standrighthere/ViewModels/UserViewModel.cs
+++ standrighthere/ViewModels/UserViewModel.cs
@@ -4,14 +4,16 @@ using Parse;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using standrighthere.Utilities;
+using System.ComponentModel;
 
 namespace standrighthere.ViewModels
 {
-    public partial class UserViewModel
+    public partial class UserViewModel : INotifyPropertyChanged
     {
         public UserViewModel(ParseUser user)
         {
             _user = user;
+            FetchData();
         }
 
         public string Username
@@ -22,63 +24,41 @@ namespace standrighthere.ViewModels
             }
         }
 
-        public int SolvedCount
-        {
-            get
-            {
-                return (from challenge in ParseObject.GetQuery("Challenges")
-                        where challenge.Get<ParseUser>("user") == _user
-                        select challenge).CountAsync().Result;
+        public int SolvedCount { get; set; }
 
-            }
-        }
-        
-        public int SubmittedCount
-        {
-            get
-            {
-                return (from challenge in ParseObject.GetQuery("UserChallengesSolved")
-                        where challenge.Get<ParseUser>("user") == _user
-                        select challenge).CountAsync().Result;
-                
-            }
-        }
+        public int SubmittedCount { get; set; }
         
         public string CreatedRelative
         {
             get
             {
                 return TimeAgo.GetTimeAgo(_user.CreatedAt.Value);
-                
             }
         }
-/*
-        public ObservableCollection<ChallengeViewModel> SolvedChallenges { get; private set; }
 
-        public ObservableCollection<ChallengeViewModel> SubmittedChallenges { get; private set; }
-
-        public bool IsDataLoaded { get; private set; }
-
-        public async Task LoadData()
+        async Task FetchData()
         {
-            var solvedChallenges = await (from challenge in ParseObject.GetQuery("Challenges")
-                                      where challenge.Get<ParseUser>("user") == _user
-                                      select challenge).FindAsync();
-            foreach (var challenge in solvedChallenges)
-            {
-                SolvedChallenges.Add(new ChallengeViewModel(challenge));
-            }
+            SolvedCount = await (from challenge in ParseObject.GetQuery("Challenges")
+                                 where challenge.Get<ParseUser>("user") == _user
+                                 select challenge).CountAsync();
+            NotifyPropertyChanged("SolvedCount");
 
-            var submittedChallenges = await (from userChallenge in ParseObject.GetQuery("UserChallengesSolved")
-                                         where userChallenge.Get<ParseUser>("user") == _user
-                                         select userChallenge).FindAsync();
-            foreach (var challenge in submittedChallenges)
-            {
-                SubmittedChallenges.Add(new ChallengeViewModel(challenge));
-            }
-            IsDataLoaded = true;
+            SubmittedCount = await (from challenge in ParseObject.GetQuery("UserChallengesSolved")
+                                    where challenge.Get<ParseUser>("user") == _user
+                                    select challenge).CountAsync();
+            NotifyPropertyChanged("SubmittedCount");
         }
-*/
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
         private ParseUser _user;
     }
 }
