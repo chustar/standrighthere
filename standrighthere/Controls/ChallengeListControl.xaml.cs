87d0e8ea1b1e815e6c0b146f56bd6a2e3c8a00f0
Chuma Nnaji <cnnaji2003@gmail.com>
2014-01-11 19:11:38 -0800

    Basic Deisgn Data

--- standrighthere/Controls/ChallengeListControl.xaml.cs
+++ standrighthere/Controls/ChallengeListControl.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using standrighthere.ViewModels;
+using System.ComponentModel;
 
 namespace standrighthere.Controls
 {
@@ -20,14 +21,17 @@ namespace standrighthere.Controls
 
         private async void LongListSelector_ItemRealized(object sender, ItemRealizationEventArgs e)
         {
-            var challengeListViewModel = App.ViewModel.ChallengeListViewModel;
-            if (!challengeListViewModel.IsDataLoading && Challenges.ItemsSource != null && Challenges.ItemsSource.Count >= challengeListViewModel.CurrentlyLoaded)
+            if (!DesignerProperties.IsInDesignTool)
             {
-                if (e.ItemKind == LongListSelectorItemKind.Item)
+                var challengeListViewModel = App.ViewModel.ChallengeListViewModel;
+                if (!challengeListViewModel.IsDataLoading && Challenges.ItemsSource != null && Challenges.ItemsSource.Count >= challengeListViewModel.CurrentlyLoaded)
                 {
-                    if ((e.Container.Content as ChallengeViewModel).Equals(Challenges.ItemsSource[Challenges.ItemsSource.Count - challengeListViewModel.CurrentlyLoaded]))
+                    if (e.ItemKind == LongListSelectorItemKind.Item)
                     {
-                        await challengeListViewModel.LoadData(true);
+                        if ((e.Container.Content as ChallengeViewModel).Equals(Challenges.ItemsSource[Challenges.ItemsSource.Count - challengeListViewModel.CurrentlyLoaded]))
+                        {
+                            await challengeListViewModel.LoadData(true);
+                        }
                     }
                 }
             }
@@ -42,6 +46,46 @@ namespace standrighthere.Controls
                 (sender as LongListSelector).SelectedItem = null;
             }
         }
+    }
 
+}
+
+namespace Test
+{
+    public class DesignTimeData
+    {
+        public FakeChallengeList ChallengeListViewModel { get; set; }
+
+
+        public DesignTimeData()
+        {
+            ChallengeListViewModel = new FakeChallengeList();
+        }
+    }
+
+    public class FakeChallengeList
+    {
+        public FakeChallengeList()
+        {
+            Challenges = LoadCats();
+        }
+        public List<FakeCat> Challenges { get; set; }
+
+        List<FakeCat> LoadCats()
+        {
+            return new List<FakeCat>
+                       {
+                           new FakeCat {Title = "Name goes here", Image = "../Assets/fakeCat.png"},
+                           new FakeCat {Title = "Name goes here", Image = "../Assets/fakeCat.png"},
+                           new FakeCat {Title = "Name goes here", Image = "../Assets/fakeCat.png"},
+                           new FakeCat {Title = "Name goes here", Image = "../Assets/fakeCat.png"},
+                       };
+        }
+    }
+
+    public class FakeCat
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
     }
 }
